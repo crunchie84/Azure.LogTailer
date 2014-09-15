1af236b67660ee673e959e8a9d9de46fe224d300
Mark van Straten <mark@q42.nl>
2014-09-15 21:58:55 +0200

    rewrote the first call to solely be responsible for emitting new or updated logfiles

--- src/Azure.LogTailer/AzureLogTailer.cs
+++ src/Azure.LogTailer/AzureLogTailer.cs
@@ -12,30 +12,37 @@ namespace Azure.LogTailer
   /// <summary>
   /// The logical logfile tail event 'there are new bytes in this blob available to process'
   /// </summary>
-  public sealed class LogFileBlob
-  {
-    public CloudBlockBlob BlockBlob { get; set; }
-    public long ByteOffsetAlreadyProcessed { get; set; }
-    public long TotalBytesAvailableInBlob { get; set; }
-  }
+  //public sealed class LogFileBlob
+  //{
+  //  public CloudBlockBlob BlockBlob { get; set; }
+  //  public long ByteOffsetAlreadyProcessed { get; set; }
+  //  public long TotalBytesAvailableInBlob { get; set; }
+  //}
 
   public static class AzureLogTailer
   {
     /// <summary>
-    /// Starts monitoring the given blobContainer for new logfile blobs or bytes within those 
+    /// Starts monitoring the given blobContainer for new or updated logfiles
     /// within the given <paramref name="iisApplicationPrefix"/>, optionally starts with 
     /// processing at the given <paramref name="skipUntilModifiedDate"/>
     /// </summary>
-    /// <param name="blobContainer"></param>
-    /// <param name="iisApplicationPrefix"></param>
-    /// <param name="skipUntilModifiedDate">if given logfiles with a modified date before the given date are ignored</param>
-    /// <returns></returns>
-    public static IObservable<LogFileBlob> GetUnprocessedIisLogFiles(CloudBlobContainer blobContainer, string iisApplicationPrefix, DateTimeOffset? skipUntilModifiedDate)
+    /// <param name="logsBlobContainer">The blobcontainer in which the logfiles will reside</param>
+    /// <param name="iisApplicationPrefix">Within the blobcontainer the iisapp name will prefix the logfiles</param>
+    /// <param name="skipUntilModifiedDate">Logfiles with a modified date before the given date are ignored</param>
+    /// <returns>a stream of (logFile) Blobs which are created or updated</returns>
+    /// <remarks>
+    /// When subscribing to this observable it will keep state of which
+    /// blobs it has already emitted. It is the responsibility of the 
+    /// subscriber to persist this information so that upon a later 
+    /// subscription he has the information to pass a skipUntilModifiedDate
+    /// </remarks>
+    public static IObservable<CloudBlockBlob> GetNewOrModifiedIisLogFiles(CloudBlobContainer logsBlobContainer, string iisApplicationPrefix, DateTimeOffset? skipUntilModifiedDate = null)
     {
-      return Observable.Create<LogFileBlob>(observer =>
+      return Observable.Create<CloudBlockBlob>(observer =>
       {
         var lastProcessedModifiedDate = skipUntilModifiedDate ?? DateTimeOffset.MinValue;
 
+        // IIS logs are only published once every 30 seconds on Azure
         var timerObservable = Observable.Timer(TimeSpan.FromSeconds(30)).Subscribe(timer =>
         {
           //find new logfiles and publish those to the observer.onNext
@@ -43,17 +50,11 @@ namespace Azure.LogTailer
           {
             //return all files currently available in the blobstore
             // return all files in the container
-            blobContainer.ListBlobs(iisApplicationPrefix, true)
+            logsBlobContainer.ListBlobs(iisApplicationPrefix, true)
               .OfType<CloudBlockBlob>()
               .OrderBy(blob => blob.Properties.LastModified)
-              .Select(blob => new LogFileBlob
-              {
-                BlockBlob = blob,
-                ByteOffsetAlreadyProcessed = 0L,
-                TotalBytesAvailableInBlob = blob.Properties.Length
-              })
               .ToObservable()
-              .Do(newLogFileBlob => lastProcessedModifiedDate = newLogFileBlob.BlockBlob.Properties.LastModified ?? lastProcessedModifiedDate) //keep state of what we have seen
+              .Do(blob => lastProcessedModifiedDate = blob.Properties.LastModified ?? lastProcessedModifiedDate) //keep state of what we have seen
               .Subscribe(observer);
           }
           else
@@ -70,21 +71,15 @@ namespace Azure.LogTailer
                 "{0}/{1}/{2:00}/{3:00}",
                 iisApplicationPrefix, date.Year, date.Month, date.Day);
 
-              return blobContainer
+              return logsBlobContainer
                 .ListBlobs(blobContainerPrefix, true)
                 .OfType<CloudBlockBlob>()
                 .Where(blob => blob.Properties.LastModified != null && blob.Properties.LastModified > lastProcessedModifiedDate)
-                .OrderBy(blob => blob.Properties.LastModified)
-                .Select(blob => new LogFileBlob
-                {
-                  BlockBlob = blob,
-                  ByteOffsetAlreadyProcessed = 0L,//TODO this is not correct for subsequent runs
-                  TotalBytesAvailableInBlob = blob.Properties.Length
-                });
+                .OrderBy(blob => blob.Properties.LastModified);
             })
             .SelectMany(date => date)
             .ToObservable()
-            .Do(newLogFileBlob => lastProcessedModifiedDate = newLogFileBlob.BlockBlob.Properties.LastModified ?? lastProcessedModifiedDate) //keep state of what we have seen
+            .Do(blob => lastProcessedModifiedDate = blob.Properties.LastModified ?? lastProcessedModifiedDate) //keep state of what we have seen
             .Subscribe(observer);//TODO: does this onComplete the observer stream when the enumeration finishes first iteration?
           }
         });
