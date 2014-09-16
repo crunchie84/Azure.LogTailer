ea92be8480d2da70e4d2dcd523a283fd85e98267
Mark van Straten <mark@q42.nl>
2014-09-16 14:39:36 +0200

    insignificant

--- src/Azure.LogTailer/AzureLogTailer.cs
+++ src/Azure.LogTailer/AzureLogTailer.cs
@@ -10,54 +10,6 @@ namespace Azure.LogTailer
   public static class AzureLogTailer
   {
     /// <summary>
-    /// retrieve a list of blob uri prefixes to retrieve to minimize azure transactions
-    /// </summary>
-    /// <param name="iisApplicationPrefix"></param>
-    /// <param name="logsSinceModifiedDate"></param>
-    /// <returns></returns>
-    private static IEnumerable<string> getCloudContainerPrefixes(string iisApplicationPrefix, DateTimeOffset logsSinceModifiedDate)
-    {
-      if (logsSinceModifiedDate > DateTimeOffset.UtcNow.roundPrecision(TimeSpan.TicksPerHour))
-      {
-        //we only need the last hour
-        return new[]
-        {
-          String.Format(CultureInfo.InvariantCulture, "{0}/{1}",
-            iisApplicationPrefix, logsSinceModifiedDate.ToString("yyyy/MM/dd/HH", CultureInfo.InvariantCulture)
-          )
-        };
-      }
-
-      if (logsSinceModifiedDate > DateTimeOffset.UtcNow.Date.AddDays(-7))
-      {
-        //we need the last 1 .. 7 days
-        return Enumerable.Range(0, 1 + (DateTimeOffset.UtcNow - logsSinceModifiedDate).Days)
-          .Select(offset =>
-          {
-            var date = DateTimeOffset.UtcNow.Date.AddDays(offset * -1);
-            return String.Format(CultureInfo.InvariantCulture, "{0}/{1}",
-              iisApplicationPrefix, date.ToString("yyyy/MM/dd", CultureInfo.InvariantCulture)
-            );
-          });
-      }
-
-      //we need them all (or it is at least more efficient to page in memory
-      return new[] { iisApplicationPrefix };
-    }
-
-    /// <summary>
-    /// round the precision of the given datetimeoffset with the amount of <paramref name="roundTicks"/>
-    /// </summary>
-    /// <param name="date"></param>
-    /// <param name="roundTicks"></param>
-    /// <returns></returns>
-    private static DateTimeOffset roundPrecision(this DateTimeOffset date, long roundTicks)
-    {
-      return date.Subtract(TimeSpan.FromTicks(date.Ticks % roundTicks));
-    }
-
-
-    /// <summary>
     /// Starts monitoring the given blobContainer for new or updated logfiles
     /// within the given <paramref name="iisApplicationPrefix"/>, optionally starts with 
     /// processing at the given <paramref name="skipUntilModifiedDate"/>
@@ -98,6 +50,53 @@ namespace Azure.LogTailer
 
         return timerObservable;
       });
+    }
+
+    /// <summary>
+    /// retrieve a list of blob uri prefixes to retrieve to minimize azure transactions
+    /// </summary>
+    /// <param name="iisApplicationPrefix"></param>
+    /// <param name="logsSinceModifiedDate"></param>
+    /// <returns></returns>
+    private static IEnumerable<string> getCloudContainerPrefixes(string iisApplicationPrefix, DateTimeOffset logsSinceModifiedDate)
+    {
+        if (logsSinceModifiedDate > DateTimeOffset.UtcNow.roundPrecision(TimeSpan.TicksPerHour))
+        {
+            //we only need the last hour
+            return new[]
+        {
+          String.Format(CultureInfo.InvariantCulture, "{0}/{1}",
+            iisApplicationPrefix, logsSinceModifiedDate.ToString("yyyy/MM/dd/HH", CultureInfo.InvariantCulture)
+          )
+        };
+        }
+
+        if (logsSinceModifiedDate > DateTimeOffset.UtcNow.Date.AddDays(-7))
+        {
+            //we need the last 1 .. 7 days
+            return Enumerable.Range(0, 1 + (DateTimeOffset.UtcNow - logsSinceModifiedDate).Days)
+              .Select(offset =>
+              {
+                  var date = DateTimeOffset.UtcNow.Date.AddDays(offset * -1);
+                  return String.Format(CultureInfo.InvariantCulture, "{0}/{1}",
+                    iisApplicationPrefix, date.ToString("yyyy/MM/dd", CultureInfo.InvariantCulture)
+                  );
+              });
+        }
+
+        //we need them all (or it is at least more efficient to page in memory
+        return new[] { iisApplicationPrefix };
+    }
+
+    /// <summary>
+    /// round the precision of the given datetimeoffset with the amount of <paramref name="roundTicks"/>
+    /// </summary>
+    /// <param name="date"></param>
+    /// <param name="roundTicks"></param>
+    /// <returns></returns>
+    private static DateTimeOffset roundPrecision(this DateTimeOffset date, long roundTicks)
+    {
+        return date.Subtract(TimeSpan.FromTicks(date.Ticks % roundTicks));
     }
   }
 }
