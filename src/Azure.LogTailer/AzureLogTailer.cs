134bce6ba8fc51a6023ccacf7adcce9b416be200
Mark van Straten <mark@q42.nl>
2014-09-30 17:35:55 +0200

    cleanup of the function names, cleanup of iis llog json parsing

--- src/Azure.LogTailer/AzureLogTailer.cs
+++ src/Azure.LogTailer/AzureLogTailer.cs
@@ -11,12 +11,16 @@ namespace Azure.LogTailer
 {
 	public static class AzureLogTailer
 	{
-
-		public static readonly string[] iisLogFields =
+		/// <summary>
+		/// We store the exact fieldnames here for the iis log lines so we 
+		/// can match those against the values and json-serialize those.
+		/// </summary>
+		private static readonly string[] IisLogFields =
 			"date time s-sitename cs-method cs-uri-stem cs-uri-query s-port cs-username c-ip cs(User-Agent) cs(Cookie) cs(Referer) cs-host sc-status sc-substatus sc-win32-status sc-bytes cs-bytes time-taken"
 				.Split(' ')
 				.Select(f => f.Replace('(', '_').Replace(")", "").Replace('-', '_'))//remove/replace json-incompatible chars
 				.ToArray();
+
 		/// <summary>
 		/// parse the given iis logline and return it in json_event logstash compatible json
 		/// </summary>
@@ -24,43 +28,28 @@ namespace Azure.LogTailer
 		/// <returns></returns>
 		public static string ParseIisLogLineToJsonEvent(string iisLogLine)
 		{
-			//json_event format:
-			//{
-			//  "message"    => "hello world",
-			//  "@version"   => "1",
-			//  "@timestamp" => "2014-04-22T23:03:14.111Z",
-			//  "type"       => "stdin",
-			//  "host"       => "hello.local"
-			//}
+			var values = iisLogLine.Split(' ').ToArray();
+			var keyValuePairs = IisLogFields
+				.Skip(2) //skip the date and time
+				.Select((value, index) => new { index, field = value })
+				.Zip(
+					iisLogLine.Split(' ')
+						.Skip(2)
+						.Select(val => val.Replace(@"""", "'")), // escape some json-invalid stuff
+					(keyTuple, value) =>
+					{
+						if (keyTuple.index > 10 || keyTuple.index == 4)//zero based index 4 = port, all above 10 = bytes/response times etc
+							return String.Format(CultureInfo.InvariantCulture, @"""{0}"":{1}", keyTuple.field, value);//int values
+						return String.Format(CultureInfo.InvariantCulture, @"""{0}"":""{1}""", keyTuple.field, value);//string values
+					});
+
+			var timestamp = DateTime.ParseExact(values[0] + " " + values[1], "yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal);
 			//@timestamp is the ISO8601 high-precision timestamp for the event.
 			//@version is the version number of this json schema
 			// all other fields are free
-			
-			//iis log line format
-			//#Fields: date time s-sitename cs-method cs-uri-stem cs-uri-query s-port cs-username c-ip cs(User-Agent) cs(Cookie) cs(Referer) cs-host sc-status sc-substatus sc-win32-status sc-bytes cs-bytes time-taken
-			//index		  0	    1		  2			3		4				5		6		7			8	9				10			11			12		13			14			15				16		17		18
-			var values = iisLogLine.Split(' ').ToArray();
-
-      //string values
-		  var keyValuePairs = iisLogFields
-		    .Skip(2) //skip the date and time
-		    .Select((value, index) => new {index, field = value})
-		    .Zip(
-		      values
-		        .Skip(2)
-		        .Select(val => val.Replace(@"""", "'")), // escape some json-invalid stuff
-		      (keyTuple, value) =>
-		      {
-		        if (keyTuple.index > 10 || keyTuple.index == 4)//zero based index 4 = port, all above 10 = bytes/response times etc
-		          return String.Format(CultureInfo.InvariantCulture, @"""{0}"":{1}", keyTuple.field, value);//int values
-		        return String.Format(CultureInfo.InvariantCulture, @"""{0}"":""{1}""", keyTuple.field, value);//string values
-		      });
-
-			//2014-09-10 02:54:41
-			var timestamp = DateTime.ParseExact(values[0] +" "+ values[1], "yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal);
 
 			return string.Format(CultureInfo.InvariantCulture,
-				@"{{""@version"":1, ""@timestamp"":""{0}"",""type"":""iis"",{1}}}", 
+				@"{{""@version"":1, ""@timestamp"":""{0}"",""type"":""iis"",{1}}}",
 					timestamp.ToString("yyyy-MM-ddTHH:mm:ssK"),
 					String.Join(",", keyValuePairs)
 				);
@@ -82,12 +71,11 @@ namespace Azure.LogTailer
 		{
 			var bytesPerUriAlreadyProcessed = new Dictionary<string, long>();
 
-			return GetNewOrModifiedIisLogFiles(logsBlobContainer, iisApplicationPrefix, skipUntilModifiedDate)
-	#if DEBUG
-				//.Do(logFile => Console.WriteLine("new or updated file: " + logFile.Uri))
-				.Do(_ => Console.Write("X"))
-	#endif
-				.Select(newOrModifiedLogFile => Observable.Using(
+			return GetNewOrModifiedLogFiles(logsBlobContainer, iisApplicationPrefix, skipUntilModifiedDate)
+#if DEBUG
+.Do(logFile => Console.WriteLine("new or updated file: " + logFile.Uri))
+#endif
+.Select(newOrModifiedLogFile => Observable.Using(
 					() =>
 					{
 						// skip/seek the unprocessed parts
@@ -107,47 +95,34 @@ namespace Azure.LogTailer
 						.Where(line => !line.StartsWith("#"))
 						.Select(line => line.Replace("~1", ""))
 					))
- 				.SelectMany(l => l);
-		}
-
-    //public static IObservable<byte> ToObservable(this MemoryStream source, 
-    //  int buffersize, 
-    //  IScheduler scheduler)
-    //  {
-    //  var bytes = Observable.Create<byte>(o =>
-    //  {
-    //    //...
-    //  });
-    //  return Observable.Using(() => source, _ => bytes);
-    //}
-
-		private static IObservable<string> readLinesFromStream(StreamReader stream)
-		{
-			return Observable.Create<string>(observer =>
-			{
-				while (!stream.EndOfStream)
-					observer.OnNext(stream.ReadLine());
-
-				return stream;
-			});
-		}
-
-		private static IEnumerable<string> readLines(Stream stream)
-		{
-			using (var reader = new StreamReader(stream))
-			{
-				while (!reader.EndOfStream)
-					yield return reader.ReadLine();
-			}
+				.SelectMany(l => l);
 		}
 
 		/// <summary>
-		/// Starts monitoring the given blobContainer for new or updated logfiles
-		/// within the given <paramref name="iisApplicationPrefix"/>, optionally starts with 
+		/// return an observable which emits strings from the given streamreader
+		/// </summary>
+		/// <param name="streamReader"></param>
+		/// <returns></returns>
+		/// <remarks>
+		/// Does not dispose the given streamReader
+		/// </remarks>
+		private static IObservable<string> readLinesFromStream(StreamReader streamReader)
+		{
+			return Observable.Create<string>(observer =>
+			{
+				while (!streamReader.EndOfStream)
+					observer.OnNext(streamReader.ReadLine());
+				return streamReader;
+			});
+		}
+
+		/// <summary>
+		/// Starts monitoring the given blobContainer for new or updated CloubBlockBlobs (logs)
+		/// within the given <paramref name="applicationPrefix"/>, optionally starts with 
 		/// processing at the given <paramref name="skipUntilModifiedDate"/>
 		/// </summary>
 		/// <param name="logsBlobContainer">The blobcontainer in which the logfiles will reside</param>
-		/// <param name="iisApplicationPrefix">Within the blobcontainer the iisapp name will prefix the logfiles</param>
+		/// <param name="applicationPrefix">Within the blobcontainer the iisapp name will prefix the logfiles or the application logs</param>
 		/// <param name="skipUntilModifiedDate">Logfiles with a modified date before the given date are ignored</param>
 		/// <returns>a stream of (logFile) Blobs which are created or updated</returns>
 		/// <remarks>
@@ -156,7 +131,7 @@ namespace Azure.LogTailer
 		/// subscriber to persist this information so that upon a later 
 		/// subscription he has the information to pass a skipUntilModifiedDate
 		/// </remarks>
-		public static IObservable<CloudBlockBlob> GetNewOrModifiedIisLogFiles(CloudBlobContainer logsBlobContainer, string iisApplicationPrefix, DateTimeOffset? skipUntilModifiedDate = null)
+		public static IObservable<CloudBlockBlob> GetNewOrModifiedLogFiles(CloudBlobContainer logsBlobContainer, string applicationPrefix, DateTimeOffset? skipUntilModifiedDate = null)
 		{
 			return Observable.Create<CloudBlockBlob>(observer =>
 			{
@@ -165,19 +140,19 @@ namespace Azure.LogTailer
 
 				// IIS logs are only published once every 30 seconds on Azure
 				var timerObservable = Observable.Timer(TimeSpan.FromSeconds(30))
-				  .StartWith(-1L)//immediatly fire first event
-				  .Subscribe(_ =>
-					getCloudContainerPrefixes(iisApplicationPrefix, lastProcessedModifiedDate)
-					  .Select(prefix => logsBlobContainer.ListBlobs(prefix, true)
+					.StartWith(-1L)//immediatly fire first event
+					.Subscribe(_ =>
+					getCloudContainerPrefixes(applicationPrefix, lastProcessedModifiedDate)
+						.Select(prefix => logsBlobContainer.ListBlobs(prefix, true)
 						.OfType<CloudBlockBlob>()
 						.Where(
-						  blob => blob.Properties.LastModified != null && blob.Properties.LastModified > lastProcessedModifiedDate)
-					  )
-					  .SelectMany(blobs => blobs)
-					  .OrderBy(blob => blob.Properties.LastModified)
-					  .ToObservable()
-					  .Do(blob => lastProcessedModifiedDate = blob.Properties.LastModified ?? lastProcessedModifiedDate)
-					  .Subscribe(observer.OnNext, observer.OnError)
+							blob => blob.Properties.LastModified != null && blob.Properties.LastModified > lastProcessedModifiedDate)
+						)
+						.SelectMany(blobs => blobs)
+						.OrderBy(blob => blob.Properties.LastModified)
+						.ToObservable()
+						.Do(blob => lastProcessedModifiedDate = blob.Properties.LastModified ?? lastProcessedModifiedDate)
+						.Subscribe(observer.OnNext, observer.OnError)
 					);
 
 				return timerObservable;
@@ -187,37 +162,37 @@ namespace Azure.LogTailer
 		/// <summary>
 		/// retrieve a list of blob uri prefixes to retrieve to minimize azure transactions
 		/// </summary>
-		/// <param name="iisApplicationPrefix"></param>
+		/// <param name="appliationPrefix">Can be the IIS app or application_logs prefix</param>
 		/// <param name="logsSinceModifiedDate"></param>
 		/// <returns></returns>
-		private static IEnumerable<string> getCloudContainerPrefixes(string iisApplicationPrefix, DateTimeOffset logsSinceModifiedDate)
+		private static IEnumerable<string> getCloudContainerPrefixes(string appliationPrefix, DateTimeOffset logsSinceModifiedDate)
 		{
 			if (logsSinceModifiedDate > DateTimeOffset.UtcNow.roundPrecision(TimeSpan.TicksPerHour))
 			{
 				//we only need the last hour
 				return new[]
-				{
-					String.Format(CultureInfo.InvariantCulture, "{0}/{1}",
-						iisApplicationPrefix, logsSinceModifiedDate.ToString("yyyy/MM/dd/HH", CultureInfo.InvariantCulture)
-					)
-				};
+        {
+          String.Format(CultureInfo.InvariantCulture, "{0}/{1}",
+            appliationPrefix, logsSinceModifiedDate.ToString("yyyy/MM/dd/HH", CultureInfo.InvariantCulture)
+          )
+        };
 			}
 
 			if (logsSinceModifiedDate > DateTimeOffset.UtcNow.Date.AddDays(-7))
 			{
 				//we need the last 1 .. 7 days
 				return Enumerable.Range(0, 1 + (DateTimeOffset.UtcNow - logsSinceModifiedDate).Days)
-				  .Select(offset =>
-				  {
-					  var date = DateTimeOffset.UtcNow.Date.AddDays(offset * -1);
-					  return String.Format(CultureInfo.InvariantCulture, "{0}/{1}",
-						iisApplicationPrefix, date.ToString("yyyy/MM/dd", CultureInfo.InvariantCulture)
-					  );
-				  });
+					.Select(offset =>
+					{
+						var date = DateTimeOffset.UtcNow.Date.AddDays(offset * -1);
+						return String.Format(CultureInfo.InvariantCulture, "{0}/{1}",
+						appliationPrefix, date.ToString("yyyy/MM/dd", CultureInfo.InvariantCulture)
+						);
+					});
 			}
 
 			//we need them all (or it is at least more efficient to page in memory
-			return new[] { iisApplicationPrefix };
+			return new[] { appliationPrefix };
 		}
 
 		/// <summary>
